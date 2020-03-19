9ed94fff8fe74a50c8da674e201d3fcba8229ef7
aaron-mcgrath-adp <aaron.mcgrath@adaptris.com>
2020-03-19 10:42:10 +0000

    INTERLOK-3210 Added a few more tests.

--- src/test/java/com/adaptris/mgmt/cluster/mbean/ClusterManagerTest.java
+++ src/test/java/com/adaptris/mgmt/cluster/mbean/ClusterManagerTest.java
@@ -1,21 +1,41 @@
 package com.adaptris.mgmt.cluster.mbean;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import com.adaptris.core.CoreException;
+import com.adaptris.core.cache.ExpiringMapCache;
 import com.adaptris.mgmt.cluster.ClusterInstance;
 
 public class ClusterManagerTest {
   
   private ClusterManager clusterManager;
   
+  @Mock private ExpiringMapCache mockClusterInstances;
+  
+  private List<String> mockKeySet;
+  
   @BeforeEach
   public void setUp() throws Exception {
+    MockitoAnnotations.initMocks(this);
     clusterManager = new ClusterManager();
+    
+    mockKeySet = new ArrayList<>();
+    mockKeySet.add("key");
   }
   
   @Test
@@ -38,6 +58,25 @@ public class ClusterManagerTest {
   }
   
   @Test
+  public void testClusterManagerMultipleNewInstanceNotReAdded() throws Exception {
+    clusterManager.setDebug(true);
+    UUID randomUUID = UUID.randomUUID();
+    
+    ClusterInstance clusterInstance1 = new ClusterInstance(randomUUID, "MyAdapterId", "myJmxAddress");
+    ClusterInstance clusterInstance2 = new ClusterInstance(randomUUID, "MyAdapterId2", "myJmxAddress2");
+    
+    clusterManager.clusterInstancePinged(clusterInstance1);
+    clusterManager.clusterInstancePinged(clusterInstance2);
+    clusterManager.clusterInstancePinged(clusterInstance1);
+    clusterManager.clusterInstancePinged(clusterInstance2);
+    
+    System.out.println(clusterManager.getKnownClusterInstancesAsString());
+    
+    assertTrue(clusterManager.getKnownClusterInstancesAsString().indexOf("myJmxAddress") >= 0);
+    assertTrue(clusterManager.getKnownClusterInstancesAsString().indexOf("myJmxAddress2") >= 0);
+  }
+  
+  @Test
   public void testClusterManagerMultipleNewInstance() throws Exception {
     clusterManager.setDebug(true);
     clusterManager.clusterInstancePinged(new ClusterInstance(UUID.randomUUID(), "MyAdapterId", "myJmxAddress"));
@@ -48,6 +87,37 @@ public class ClusterManagerTest {
     assertTrue(clusterManager.getKnownClusterInstancesAsString().indexOf("myJmxAddress") >= 0);
     assertTrue(clusterManager.getKnownClusterInstancesAsString().indexOf(",") > 0);
     assertTrue(clusterManager.getKnownClusterInstancesAsString().indexOf("myJmxAddress2") >= 0);
+  }
+  
+  @Test
+  public void testClusterManagerNoMap() throws Exception {
+    doThrow(new CoreException("expected"))
+        .when(mockClusterInstances).getKeys();
+        
+    clusterManager.setClusterInstances(mockClusterInstances);
+    
+    try {
+      assertNull(clusterManager.getKnownClusterInstancesAsString());
+    } catch (Exception ex) {
+      fail("Should return null and fail silently");
+    }
+  }
+  
+  @Test
+  public void testClusterManagerKeyFails() throws Exception {
+    // Should be impossible, but expiring map cache apparently can throw an exception here...
+    when(mockClusterInstances.getKeys())
+        .thenReturn(mockKeySet);
+    doThrow(new CoreException("expected"))
+        .when(mockClusterInstances).get(anyString());
+    
+    clusterManager.setClusterInstances(mockClusterInstances);
+    
+    try {
+      assertEquals("", clusterManager.getKnownClusterInstancesAsString());
+    } catch (Exception ex) {
+      fail("Should return null and fail silently");
+    }
   }
 
 }
