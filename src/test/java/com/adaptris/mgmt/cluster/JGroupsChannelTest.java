1dcdb5c2ecd4ecc0051206528c5f3dc12d5bfed4
Sebastien Belin <sebastien.belin@proagrica.com>
2023-05-18 11:55:56 +0900

    Upgrade gradle wrapper to 7.6.1

--- src/test/java/com/adaptris/mgmt/cluster/JGroupsChannelTest.java
+++ src/test/java/com/adaptris/mgmt/cluster/JGroupsChannelTest.java
@@ -2,12 +2,12 @@ package com.adaptris.mgmt.cluster;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.jgroups.JChannel;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -16,26 +16,34 @@ import org.mockito.MockitoAnnotations;
 import com.adaptris.mgmt.cluster.jgroups.JGroupsChannel;
 
 public class JGroupsChannelTest {
-  
+
   private static final String CLUSTER_NAME = "MyCluster";
-  
+
   private JGroupsChannel jGroupsChannel;
-  
-  @Mock private JChannel mockJChannel;
-  
+
+  @Mock
+  private JChannel mockJChannel;
+
+  private AutoCloseable mock;
+
   @BeforeEach
   public void setUp() throws Exception {
-    MockitoAnnotations.initMocks(this);
+    mock = MockitoAnnotations.openMocks(this);
+  }
+
+  @AfterEach
+  public void tearDown() throws Exception {
+    mock.close();
   }
 
   /************************************************************************
-   *******************   TESTS   ****************************************** 
+   ******************* TESTS ******************************************
    ************************************************************************/
-  
+
   @Test
   public void testSingleton() throws Exception {
     jGroupsChannel = JGroupsChannel.getInstance();
-    
+
     assertNotNull(jGroupsChannel.getJGroupsChannel());
   }
 
@@ -43,47 +51,47 @@ public class JGroupsChannelTest {
   public void testSingletonJChannelNotRecreated() throws Exception {
     jGroupsChannel = JGroupsChannel.getInstance();
     JChannel secondCallChannel = JGroupsChannel.getInstance().getJGroupsChannel();
-    
+
     assertTrue(jGroupsChannel.getJGroupsChannel() == secondCallChannel);
   }
-  
+
   @Test
   public void testStartUp() throws Exception {
     jGroupsChannel = JGroupsChannel.getInstance();
     jGroupsChannel.setJGroupsChannel(mockJChannel);
     jGroupsChannel.setClusterName(CLUSTER_NAME);
-    
+
     jGroupsChannel.start();
-    
+
     verify(mockJChannel).connect(CLUSTER_NAME);
   }
-  
+
   @Test
   public void testShutDown() throws Exception {
     when(mockJChannel.isConnected())
         .thenReturn(true);
-    
+
     jGroupsChannel = JGroupsChannel.getInstance();
     jGroupsChannel.setJGroupsChannel(mockJChannel);
-    
+
     jGroupsChannel.stop();
-    
+
     verify(mockJChannel).disconnect();
     verify(mockJChannel).close();
   }
-  
+
   @Test
   public void testShutdownSilently() throws Exception {
     when(mockJChannel.isConnected())
         .thenReturn(false);
-    
+
     jGroupsChannel = JGroupsChannel.getInstance();
     jGroupsChannel.setJGroupsChannel(mockJChannel);
-    
+
     jGroupsChannel.stop();
-    
+
     verify(mockJChannel, times(0)).disconnect();
     verify(mockJChannel, times(0)).close();
   }
-  
+
 }
